9544e262ab59404447485328bd1c88358add1755
unknown <cristiana.antalut@gmail.com>
2018-11-15 01:36:50 +0200

    refactor structure

---
+++ Front-End/src/app/file-resolved/file-resolved.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-resolved',
+  templateUrl: './file-resolved.component.html',
+  styleUrls: ['./file-resolved.component.css']
+})
+export class FileResolvedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
