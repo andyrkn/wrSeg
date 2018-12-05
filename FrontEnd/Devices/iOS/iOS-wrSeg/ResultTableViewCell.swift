93b2b024d2a001a2e4ffc11589ae0ddf94daf1b5
Virga Cosmin <virgacosmin@gmail.com>
2018-12-05 23:34:29 +0200

    Advanced settings

---
+++ FrontEnd/Devices/iOS/iOS-wrSeg/ResultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultTableViewCell.swift
+//  iOS-wrSeg
+//
+//  Created by v7808911 on 04/12/2018.
+//  Copyright © 2018 Amazon. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
