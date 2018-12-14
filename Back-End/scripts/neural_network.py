77d33cec83b79c68e895889d8e0afdea5d639f0f
loghinalexandru <loghinalexandru61@gmail.com>
2018-12-14 20:47:25 +0200

    Refactoring

--- Back-End/scripts/neural_network.py
+++ Back-End/scripts/neural_network.py
@@ -4,55 +4,71 @@ from keras.models import Sequential
 from keras.layers import InputLayer, Dense , Dropout
 
 
-train_data_input = []
-train_data_label = []
+def build_model():
+    model = keras.models.Sequential()
+    model.add(InputLayer((4,))) # tuple(x,y,height,width , nearest neighbour distance)
+    model.add(Dense(50 , activation="tanh"))
+    model.add(Dense(100 , activation="tanh"))
+    model.add(Dropout(0.50))
+    model.add(Dense(50, activation="relu"))
+    model.add(Dense(5 , activation="softmax")) # adnotation , title , content, footer , page number
+    model.compile(optimizer = keras.optimizers.Adam(), loss = "categorical_crossentropy", metrics = ["accuracy"])
+    return model;
 
-def get_labels():
-    switcher = {
-            0: "Adnotation",
-            1: "Title",
-            2: "Content",
-            3: "Footer",
-            4: "Page Number",
-    }
-    data = []
+
+def save_model(model):
+	model.save_weights("weights.h5")
+
+
+def load_model(model):
+	model.load_weights("weights.h5")
+	model.compile(keras.optimizers.Adam(), loss = "categorical_crossentropy", metrics = ["accuracy"])
+	return model
+
+
+def read_test_data():
     input = []
     with open("test_data.txt" , "r") as fd:
         for line in fd:
             line_split = line.replace("\n" , "").split(" ")
-            input.append(line_split[:1])
-            data.append(list(map(float , line_split[1:])))
-    labels = model.predict_classes(np.array(data))
-    for i in range(len(input)):
-        print(input[i] , switcher[labels[i]])
+            input.append(list(map(float , line_split)))
+    return input
+
+def predict_and_write(model , test_data_input):
+    lines = []
+    switcher = {
+                0: "Adnotation",
+                1: "Title",
+                2: "Content",
+                3: "Footer",
+                4: "Page Number",
+    }
+    test_data_label = model.predict_classes(np.array(test_data_input))
+    with open("column_indexes.txt", 'r') as fd:     # load file
+        lines = fd.read().splitlines()
+    with open("column_indexes.txt" , 'w') as fd:
+        for i in range(len(lines)):
+            fd.write(lines[i] + " " + switcher[test_data_label[i]] + "\n")
+
+def read_training_data():
+    data = []
+    input = []
+    label = []
+    with open("train_data.txt" , "r") as fd:
+        for line in fd:
+            data = line.replace("\n" , "").split(" ")
+            input.append(list(map(float , data[0:4])))
+            label.append(list(map(float , data[4:]))[0])
+    return (np.array(input) , keras.utils.to_categorical(label, num_classes = 5))
 
 
-with open("train_data.txt" , "r") as fd:
-    for line in fd:
-        data = line.replace("\n" , "").split(" ")
-        input = list(map(float , data[1:6]))
-        label = list(map(float , data[6:]))
-        train_data_input.append(input)
-        train_data_label.append(label[0])
-
-train_data_input = np.array(train_data_input)
-train_data_label = keras.utils.to_categorical(train_data_label, num_classes = 5)
-
-model = keras.models.Sequential()
-model.add(InputLayer((5,))) # tuple(x,y,height,width , nearest neighbour distance)
-model.add(Dense(500 , activation="tanh"))
-model.add(Dense(800 , activation="tanh"))
-model.add(Dropout(0.50))
-model.add(Dense(500, activation="relu"))
-model.add(Dense(5 , activation="softmax")) # adnotation , title , content, footer , page number
-
-
-model.compile(optimizer = keras.optimizers.SGD(lr = 0.05), loss = "categorical_crossentropy", metrics = ["accuracy"])
-
-model.fit(train_data_input, train_data_label, epochs = 300,  batch_size = 10)
-
-# model.save("test_model.h5")
-
-# model = keras.models.load_model("test_model.h5")
-
-get_labels()
+if __name__ == "__main__":
+    train_data_input = []
+    train_data_label = []
+    model  = build_model()
+    # model = load_model(build_model())
+    train_data_input , train_data_label = read_training_data()
+    model.fit(train_data_input, train_data_label, epochs = 300 ,  batch_size = 20)
+    # save_model(model)
+    test_data = read_test_data()
+    predict_and_write(model , test_data)
--- Back-End/scripts/write_data.py
+++ Back-End/scripts/write_data.py
@@ -1,23 +1,22 @@
 import sys
-from classify import minDist
 import cv2
 
-
 def write_data(binary, path):
-    shape = cv2.imread(binary).shape[:2]
-    with open(path , "r") as file_input , open("train_data.txt" , "a") as file_output:
+    shape = cv2.imread(binary).shape[:2][::-1]
+    with open(path , "r") as file_input , open("test_data.txt" , "w") as file_output:
         input_data = []
         for line in file_input:
             data = []
             data = line.replace("\n" , " ").split()
-            path = data[0:1]
-            features = list(map(int ,data[1:]))
+            features = list(map(float ,data))
+            features[2] -= features[0] # get width
+            features[3] -= features[1] # get height
             features = [features[i] / shape[i % 2] for i in range(len(features))] # Normalize
-            input_data.append(path + features)
+            input_data.append(features)
         for line in input_data:
-            for elem in line:
-                file_output.write(str(elem) + " ")
-            file_output.write(str(minDist(line , input_data)) + "\n")
+            for i in range(len(line) - 1):
+                file_output.write(str(line[i]) + " ")
+            file_output.write(str(line[len(line) - 1]) + "\n")
 
 if __name__ == "__main__":
     write_data(sys.argv[1],sys.argv[2])
