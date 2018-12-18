b2b3ea1506bdf64ea38cca1b444498754eea10b8
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2018-12-18 15:48:27 +0200

    create images from server response

--- Test/segment comparer/server_request.py
+++ Test/segment comparer/server_request.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import image_tools
+import cv2
 
 def json_from_path(image_path):
     url = "http://localhost:8082/upload-file"
@@ -11,7 +13,13 @@ def dict_from_path(image_path):
     json_string = json_from_path(image_path)
     return json.loads(json_string)
 
+def segment_images_from_path(image_path, folder_path):
+    coords_dict = dict_from_path(image_path)
+    segment_images = image_tools.images_dict_from_coords_dict(coords_dict, image_path)
+    return segment_images
+
 if __name__ == "__main__":
     image_path = './tests/json/testpage.png'
-    print(type(json_from_path(image_path)))
-    print(type(dict_from_path(image_path)))
+    folder_path = './tests/result'
+    segment_images = segment_images_from_path(image_path, folder_path)
+    image_tools.write_images_from_dict(segment_images, folder_path)
