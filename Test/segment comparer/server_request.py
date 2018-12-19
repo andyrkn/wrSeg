f516a5f2fca1f4c12be55f6a31e593d813bd2f37
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2018-12-19 16:04:45 +0200

    write on succes  and  exception catching

--- Test/segment comparer/server_request.py
+++ Test/segment comparer/server_request.py
@@ -3,11 +3,20 @@ import json
 import image_tools
 import os
 
+class BadRequestException(Exception):
+    def __init__(self, message, response=None):
+        super().__init__(message)
+        self.response = response
+
+
 def json_from_path(image_path):
     url = "http://localhost:8082/upload-file"
     files = {'file': open(image_path, 'rb')}
     response = requests.post(url, files=files, verify=False)
-    return response.text
+    if response.ok:
+        return response.text
+    else:
+        raise BadRequestException("Bad request", response=response)
 
 def coords_dict_from_path(image_path):
     json_string = json_from_path(image_path)
@@ -18,31 +27,42 @@ def segment_images_from_path(image_path):
     segment_images = image_tools.images_dict_from_coords_dict(coords_dict, image_path)
     return segment_images
 
-def segment_images_from_folder_path(folder_path):
+def segment_images_from_folder_path(folder_path, on_success_listeners=None):
     folder_dict = dict()
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
         if os.path.isfile(file_path):
+            print("{}:".format(file_name))
             try:
                 segment_images = segment_images_from_path(file_path)
                 if segment_images:
                     folder_dict[file_name] = segment_images
+                    print("successful")
+                    if on_success_listeners:
+                        for listener in on_success_listeners:
+                            listener(file_name, segment_images)
                 else:
-                    print("{}: empty folder".format(file_name))
+                    print("empty folder")
+            except BadRequestException as e:
+                print("bad request:\n{}".format(e.response))
             except Exception as e:
-                print("{}: exception: {}".format(file_name, e))
+                print("exception: {}".format(e))
     return folder_dict
 
+def create_segments_folder(folder_path, folder_name, segments_dict):
+    segments_folder_path = os.path.join(folder_path, folder_name)
+    if not os.path.exists(segments_folder_path):
+        os.makedirs(segments_folder_path)
+    image_tools.write_images_from_dict(segments_dict, segments_folder_path)
+
 if __name__ == "__main__":
-    # image_path = './tests/json/testpage.png'
+    # image_path = './tests/images/m18.png'
     # folder_path = './tests/result'
     # segment_images = segment_images_from_path(image_path)
     # image_tools.write_images_from_dict(segment_images, folder_path)
+    
     input_folder_path = './tests/images'
     output_folder_path = './tests/output_folder'
-    folder_dict = segment_images_from_folder_path(input_folder_path)
-    for folder_name in folder_dict:
-        segments_folder_path = os.path.join(output_folder_path, folder_name)
-        if not os.path.exists(segments_folder_path):
-            os.makedirs(segments_folder_path)
-        image_tools.write_images_from_dict(folder_dict[folder_name], segments_folder_path)
+    folder_dict = segment_images_from_folder_path(input_folder_path,
+                                                  [lambda x, y: create_segments_folder(output_folder_path, x, y)])
+    
