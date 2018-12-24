39dc6948ef2e747f009255d57015c3feec08ae8e
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2018-12-24 11:56:57 +0200

    create_json_folder()

--- Test/folder_tester.py
+++ Test/folder_tester.py
@@ -19,8 +19,22 @@ def create_segments_folder(folder_path, folder_name, segments_dict):
         os.makedirs(segments_folder_path)
     image_tools.write_images_from_dict(segments_dict, segments_folder_path)
 
+def create_json_folder(input_folder_path, output_folder_path):
+    for image_name in os.listdir(input_folder_path):
+        image_path = os.path.join(input_folder_path, image_name)
+        if os.path.isfile(image_path):
+            print(image_name)
+            try:
+                json_string = server_request.json_from_path(image_path)
+                with open(os.path.join(output_folder_path, image_name + ".json"), "w") as fd:
+                    fd.write(json_string)
+            except server_request.BadRequestException as e:
+                print(e.response)
+    
+
 if __name__ == "__main__":
-    input_folder_path = './tests/images'
+    input_folder_path = './images'
     output_folder_path = './tests/output_folder'
-    on_success_listeners = [lambda x, y: create_segments_folder(output_folder_path, x, y)]
-    folder_dict = server_request.segment_images_from_folder_path(input_folder_path, on_success_listeners)
+    # on_success_listeners = [lambda x, y: create_segments_folder(output_folder_path, x, y)]
+    # folder_dict = server_request.segment_images_from_folder_path(input_folder_path, on_success_listeners)
+    create_json_folder(input_folder_path, output_folder_path)
