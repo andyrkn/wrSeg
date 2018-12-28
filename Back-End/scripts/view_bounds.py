412f6c10f2aae42abb1902708f13660e24a26df2
loghinalexandru <loghinalexandru61@gmail.com>
2018-12-28 15:07:36 +0200

    Add files via upload

--- Back-End/scripts/view_bounds.py
+++ Back-End/scripts/view_bounds.py
@@ -8,7 +8,25 @@ import cv2 #pip3 install opencv-python
 import sys
 import os
 
+def write_data(binary, path):
+    shape = cv2.imread(binary).shape[:2][::-1]
+    with open(path , "r") as file_input , open("test_data.txt" , "w") as file_output:
+        input_data = []
+        for line in file_input:
+            data = []
+            data = line.replace("\n" , " ").split()
+            features = list(map(float ,data))
+            features[2] -= features[0] # get width
+            features[3] -= features[1] # get height
+            features = [features[i] / shape[i % 2] for i in range(len(features))] # Normalize
+            input_data.append(features)
+        for line in input_data:
+            for i in range(len(line) - 1):
+                file_output.write(str(line[i]) + " ")
+            file_output.write(str(line[len(line) - 1]) + "\n")
+
 if __name__ == "__main__":
+    write_data(sys.argv[1],sys.argv[2])
     user_labels = []
     reverse_switcher = {
                 "annotation": "0",
