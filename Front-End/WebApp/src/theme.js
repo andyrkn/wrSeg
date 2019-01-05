e77ead1f87491701bd12b5070c7b7e34acec3fb1
Razdran <anton.razvan1997@gmail.com>
2019-01-05 22:58:29 +0200

    Toggle button icons + Legend
    Co-Authored-By: Marian Alexandru Daniel <codrinalex1994@users.noreply.github.com>

--- Front-End/WebApp/src/theme.js
+++ Front-End/WebApp/src/theme.js
@@ -11,13 +11,32 @@ function setStyle() {
         document.getElementById("inner-circle").style.backgroundColor = 'white';
         document.getElementById("toggle-btn").style.backgroundColor = 'gray';
         document.getElementById("inner-circle").style.marginLeft = '0px';
-        document.getElementsByTagName("body")[0].style.backgroundColor='#495172';
+        document.getElementsByTagName("body")[0].style.backgroundColor = '#495172';
+        var cards = document.getElementsByClassName("card");
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].style.backgroundColor = "#313854";
+            cards[i].style.color="white";
         }
+        var element = document.getElementById("toggle-icon");
+        element.classList.remove("fa-moon");
+        element.classList.add("fa-sun");
+        element.style.color="gray";
+    }
     else {
         document.getElementById("inner-circle").style.backgroundColor = 'gray';
         document.getElementById("toggle-btn").style.backgroundColor = 'white';
         document.getElementById("inner-circle").style.marginLeft = '30px';
-        document.getElementsByTagName("body")[0].style.backgroundColor='#eee';
+        document.getElementsByTagName("body")[0].style.backgroundColor = '#eee';
+        var cards = document.getElementsByClassName("card");
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].style.backgroundColor = "#eee";
+            cards[i].style.color="black";   
         }
+        
+        var element = document.getElementById("toggle-icon");
+        element.classList.remove("fa-sun");
+        element.classList.add("fa-moon");
+        element.style.color="white";
+    }
     console.log(window.location);
 }
