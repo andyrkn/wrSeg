8ce55879d269956efd35fd59a8f2111dc4bec523
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2019-01-16 17:11:06 +0200

    pixalate.py

--- Test/image processors/image_processors.py
+++ Test/image processors/image_processors.py
@@ -13,3 +13,10 @@ def noise(image, strength=0.5):
     stds = (50, 50, 50)
     noise = cv2.randn(new_image, means, stds)
     return cv2.addWeighted(image, 1 - strength, new_image, strength, 0.0)
+
+def pixalate(image, strength=3):
+    new_image = image.copy()
+    for i in range(0, new_image.shape[0]):
+        for j in range(0, new_image.shape[1]):
+            new_image[i][j] = new_image[(i // strength) * strength][(j // strength) * strength]
+    return new_image
